76c66c4ded1e8c849ee82f80562861bb93a7114e
m moshahid ul  kallol <mmoshahidulkallol@ms-MacBook-Air.home>
2019-04-03 22:58:03 -0400

    first commit

---
+++ llpractice/llpractice.hpp
@@ -0,0 +1,14 @@
+//
+//  llpractice.hpp
+//  llpractice
+//
+//  Created by m moshahid ul  kallol on 4/3/19.
+//  Copyright © 2019 m moshahid ul  kallol. All rights reserved.
+//
+
+#ifndef llpractice_hpp
+#define llpractice_hpp
+
+#include <stdio.h>
+
+#endif /* llpractice_hpp */
